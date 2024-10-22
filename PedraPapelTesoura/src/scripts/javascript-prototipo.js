48301a9ad9c39921c7c6aadbbc5110d9cd34336d
Davi0Reboucas <davi.reboucas@dcomp.ufs.br>
2024-10-22 11:42:08 -0300

    Adição dos comentarios
    Co-Authored-By: AlexFilho-js <180571120+AlexFilho-js@users.noreply.github.com>

--- PedraPapelTesoura/src/scripts/javascript-prototipo.js
+++ PedraPapelTesoura/src/scripts/javascript-prototipo.js
@@ -1,3 +1,4 @@
+//essa função são os anexos para o html (getElementyById), sendo a parte de pontuação, as cartas, os botoes e cada jogador
 const state = {
   audio: {
       backgroundAudio: false,
@@ -26,9 +27,9 @@ const state = {
       button: document.getElementById("next-duel"),
   },
   }
-  
-  const pathImages = "./src/assets/icons/"
-  
+
+  const pathImages = "./src/assets/icons/" // mostrando em qual parte do arquivo estão os documentos para as imagens das cartas
+// neste cardData iremos mostrar todos os fundamentos da carta, desde a sua imagem, seu nome e seu atributo e para quais outras cartas ela vao ganhar ou perder sendo identificado pelo ID
   const cardData = [
   {
       id: 0,
@@ -69,19 +70,19 @@ const state = {
       img: `${pathImages}carta-lagarto-azul.png`,
       winOf: [3, 0],
       loseOf: [1, 2],
-  
+
   },
   ]
-  
+  //Essa função ela remove as cartas anteriores, oculta os campos do jogador e do computador em seguida, distribui novas cartas para o jogador e computador.
   const init = () => {
   removeAllCardsImages()
   state.fieldCards.player.style.display = "none"
   state.fieldCards.computer.style.display = "none"
-  
+
   drawCards(3, state.playerSides.player1)
   drawCards(3, state.playerSides.player2)
   }
-  
+  //Essa função desenha um número específico de cartas do lado do jogador, ela faz isso chamando-se repetidamente até que o número desejado de cartas tenha sido desenhado e exibido, adicionando cartas uma por uma a sua mão.
   const drawCards = (cardsNumber, fieldSide, acc = 0) => {
   if (acc >= cardsNumber){
       return []
@@ -91,119 +92,121 @@ const state = {
   document.getElementById(fieldSide).appendChild(cardImage)
   return  drawCards(cardsNumber, fieldSide, acc + 1 )
   }
-  
+  //A função cria um elemento de imagem para uma carta, define as propriedades e, se a carta for do player, ela permite interatividade deixando que o jogador clique e passe o mouse sobre a carta, verificando qual carta está em sua mão é seus atributos.
   const createCardImage = (card, fieldSide) => {
   const cardImage = document.createElement("img");
-  
+
   cardImage.setAttribute("height", "100px")
   cardImage.setAttribute("src", "./src/assets/icons/ufs-carta-back.png")
   cardImage.setAttribute("data-id", card.id)
   cardImage.classList.add("card");
-  
+
   if (fieldSide === state.playerSides.player1) {
       cardImage.addEventListener("click", () => {
       setCardsField(cardImage.getAttribute("data-id"))
       })
-  
+
       cardImage.addEventListener("mouseover", () => {
       drawSelectedCard(card.id)
       })
   }
-  
+
   return cardImage;
   }
-  
+  //A função e responsável por gerenciar o fluxo do duelo de cartas, entre o jogador e o computador, ela remove as cartas existentes, também obtém novas cartas para ambos, e atualiza a interface para exibir essas cartas, por fim verifica o resultado do duelo e atualiza a pontuação.
+
   const setCardsField = async(cardId) => {
   await removeAllCardsImages()
   const computerCard = await getRandomCard()
   const playerCard = cardData[cardId]
-  
+
   state.fieldCards.player.style.display = "block"
   state.fieldCards.computer.style.display = "block"
-  
+
   state.fieldCards.player.src = playerCard.img
   state.fieldCards.computer.src = computerCard.img
-  
+
   const duelResult = await checkDuelResult(playerCard, computerCard)
-  
+
   await updateScore()
   await drawButton(duelResult)
   }
-  
+  //Fusão para redefinir a interface do jogo, fazendo com que atualize os textos, limpe as imagens, oculte botões e campos de cartas, faz com que o jogo comece novamente com a interface toda limpa.
+
   const resetDuel = () => {
   state.cardSprites.name.innerText = "Selecione"
   state.cardSprites.type.innerText = "uma carta"
   state.cardSprites.avatar.src = ""
-  
+
   state.cardSprites.avatar.src = ""
   state.actions.button.style.display = "none"
-  
+
   state.fieldCards.player.style.display = "none"
   state.fieldCards.computer.style.display = "none"
-  
-  
+
+
   init()
   }
-  
+  //Essa função monstra quantas vitórias o jogador teve e quantas derrotas o computador sofreu.
   const updateScore = () => {
   state.score.scoreBox.innerText = `Vitorias: ${state.score.playerScore} | Derrotas: ${state.score.computerScore}`
   }
-  
+  //A função congela o resultado e volta para o inicio, quando o resultado for empate
   const drawButton = (result) => {
   state.actions.button.innerText = result.toUpperCase()
   state.actions.button.style.display = "block"
   }
-  
+  //Essa função determina o resultado do duelo entre duas cartas, e  atualiza a pontuação do jogador e do computador, retornando o resultado do duelo
   const  checkDuelResult = (playerCard, computerCard) => {
   let duelResult = "EMPATE"
-  
+
   if (playerCard.winOf.includes(computerCard.id)) {
       duelResult = "GANHOU"
       state.score.playerScore++
   }
-  
+
   if (playerCard.loseOf.includes(computerCard.id)) {
       duelResult = "PERDEU"
       state.score.computerScore++
   }
-  
+
   playAudio(duelResult)
   return duelResult
   }
-  
-  
+
+  //A função faz com que as cartas de sua mão sejam removidas, deixando o espaço que estavam vazios
   const removeAllCardsImages = () => {
   const player1Box = state.playerSides.player1Box
   const player2Box = state.playerSides.player2Box
-  
+
   const imgElementsPlayer1 = [...player1Box.querySelectorAll("img")]
   const imgElementsPlayer2 = [...player2Box.querySelectorAll("img")]
-  
+
   imgElementsPlayer1.map((img) => img.remove())
   imgElementsPlayer2.map((img) => img.remove())
   }
-  
-  
+
+  //A função exibe as informações de uma carta que foi selecionada na interface do jogo,assim atualizando a imagem e monstrando o nome e o tipo da carta
   const drawSelectedCard = (cardId) => {
   const selectedCard = cardData[cardId]
-  
+
   state.cardSprites.avatar.src = selectedCard.img
   state.cardSprites.name.innerText = selectedCard.name
   state.cardSprites.type.innerText = `Atributo: ${selectedCard.type}`
   }
-  
+  //Essa função seleciona e retorna uma carta aleatória do array cardData, utilizando o índice gerado aleatoriamente.
   const getRandomCard = () => {
    const randomIndex = Math.floor(Math.random() * cardData.length)
   return cardData[randomIndex]
   }
-  
+  //A função carrega e reproduz um arquivo de áudio
   const playAudio = (status) => {
   const audio = new Audio(`./src/assets/audios/${status}.wav`)
   audio.play()
   }
-  
-  init()
 
+  init()
+//A função recebe a informação que o botão foi ativado
   function botao() {
     init()
 }
